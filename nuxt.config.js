ecafca89c9c55a9da56eb8dfbe6a3f598bc1d593
Cole Hao Wu <hwa129@sfu.ca>
2021-07-18 02:32:36 -0700

    add sass and compass dependency

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    'compass-mixins'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins

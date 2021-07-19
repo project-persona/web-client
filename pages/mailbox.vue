6366633a6c53953b6b0644b95c64fc3411808b4a
Abby Wang <abilly0726@gmail.com>
2021-07-19 22:53:59 +0800

    change mail page UI

--- pages/mailbox.vue
+++ pages/mailbox.vue
@@ -1,12 +1,29 @@
 <template>
-  <v-row>
-    <v-col>
-      <h1>here goes mailbox</h1>
-    </v-col>
+  <v-row id="list" justify="center">
+    <v-expansion-panels inset>
+      <v-expansion-panel
+        v-for="mail in mailList"
+        :key="mail"
+      >
+        <v-expansion-panel-header>{{ mail.id }}</v-expansion-panel-header>
+        <v-expansion-panel-content>
+          {{ mail.text }}
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
   </v-row>
 </template>
 <script>
 export default {
-  layout: 'dashboard'
+  layout: 'dashboard',
+  data () {
+    return {
+      mailList: [
+        { id: '0', text: 'Vegetables' },
+        { id: '1', text: 'Cheese' },
+        { id: '2', text: 'Whatever else humans are supposed to eat' }
+      ]
+    }
+  }
 }
 </script>
--- pages/passwords.vue
+++ pages/passwords.vue
@@ -1,12 +1,59 @@
 <template>
-  <v-row>
-    <v-col>
-      <h1>here goes password list</h1>
-    </v-col>
-  </v-row>
+  <v-simple-table dark>
+    <template #default>
+      <thead>
+        <tr>
+          <th id="th" class="text-center">
+            SITE
+          </th>
+          <th id="th" class="text-center">
+            PASSWORD
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="pw in pwList"
+          :key="pw.site"
+        >
+          <td>{{ pw.site.toUpperCase() }}</td>
+          <td
+            class="text-center"
+          >
+            {{ pw.show ? pw.password : '#######' }} <v-btn icon class="icon" @click="pw.show = !pw.show">
+              <v-icon>
+                {{ pw.show ? 'mdi-eye' : 'mdi-eye-off' }}
+              </v-icon>
+            </v-btn>
+          </td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
 <script>
+const pwList = [
+  { password: '0', site: 'Vegetables' },
+  { password: '1', site: 'Cheese' },
+  { password: '2', site: 'Google' }
+]
+for (let i = 0; i < pwList.length; i++) {
+  pwList[i].show = false
+}
 export default {
-  layout: 'dashboard'
+  layout: 'dashboard',
+  data () {
+    return {
+      pwList
+    }
+  }
 }
 </script>
+<style>
+  #th {
+    font-size: 20px;
+  }
+  .icon {
+    float: right;
+  }
+</style>

e867b5f894b895236ff14fad46b0b754beaca0d3
Abilly-wang <abilly0726@gmail.com>
2021-08-03 07:01:49 +0800

    link all for email

--- pages/mailbox.vue
+++ pages/mailbox.vue
@@ -11,6 +11,7 @@
             align="center"
             class="spacer"
             no-gutters
+            @click="read(mail._id)"
           >
             <v-col
               cols="1"
@@ -45,129 +46,27 @@
                 </v-icon>
               </v-btn>
             </v-list-item-action>
+            <v-list-item-icon>
+              <v-icon class="icon" :color="mail.read ? 'deep-purple accent-4' : 'grey'">
+                mdi-message-outline
+              </v-icon>
+            </v-list-item-icon>
           </v-row>
         </v-expansion-panel-header>
         <v-expansion-panel-content>
-          <p>FROM: {{ mail.from.address }}</p>
-          <p>TO: {{ mail.to.address }}</p>
-          <p>date: {{ mail.date }}</p>
+          <p>FROM: {{ mail.from[0].address }}</p>
+          <p>TO: {{ mail.to[0].address }}</p>
+          <p>date: {{ mail.date | durationFormat }}</p>
           <p>subject: {{ mail.subject }}</p>
           <hr>
-          <div class="content">
-            {{ mail.content }}
-          </div>
+          <div class="content" v-html="mail.content" />
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
-    <p />
-    <p />
-    <v-row dense justify="center">
-      <v-dialog
-        v-model="dialog"
-        fullscreen
-        hide-overlay
-        transition="dialog-bottom-transition"
-      >
-        <template #activator="{ on, attrs }">
-          <v-btn
-            v-bind="attrs"
-            v-on="on"
-          >
-            Send an Email
-          </v-btn>
-        </template>
-        <v-card>
-          <v-toolbar
-            dark
-          >
-            <v-btn
-              icon
-              dark
-              @click="cancel"
-            >
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-            <v-toolbar-title>Send an Email</v-toolbar-title>
-            <v-spacer />
-            <v-toolbar-items>
-              <v-btn
-                dark
-                text
-                bottom
-                @click="create"
-              >
-                Save
-              </v-btn>
-            </v-toolbar-items>
-          </v-toolbar>
-          <v-form
-            ref="form"
-            lazy-validation
-          >
-            <v-container fluid>
-              <v-row justify="center">
-                <v-col cols="2">
-                  <v-subheader>To:</v-subheader>
-                </v-col>
-                <v-col cols="9">
-                  <v-text-field
-                    label="Email Address"
-                    outlined
-                  />
-                </v-col>
-              </v-row>
-
-              <v-row justify="center">
-                <v-col cols="2">
-                  <v-subheader>Subject:</v-subheader>
-                </v-col>
-                <v-col cols="9">
-                  <v-text-field
-                    label="Subject"
-                    outlined
-                  />
-                </v-col>
-              </v-row>
-
-              <v-row justify="center">
-                <v-col cols="11">
-                  <v-textarea
-                    label="Email Content:"
-                    outlined
-                  />
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-form>
-        </v-card>
-      </v-dialog>
-      <v-snackbar
-        v-model="snackbar"
-        :multi-line="true"
-      >
-        {{ snackbarMsg }}
-
-        <template #action="{ attrs }">
-          <v-btn
-            color="warning"
-            text
-            v-bind="attrs"
-            @click="snackbar = false"
-          >
-            Close
-          </v-btn>
-        </template>
-      </v-snackbar>
-      <v-overlay :value="overlay" :absolute="true">
-        <v-progress-circular
-          indeterminate
-          color="amber"
-        />
-      </v-overlay>
-    </v-row>
   </v-row>
 </template>
 <script>
+/*
 function getNow () {
   const today = new Date()
   const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
@@ -175,7 +74,7 @@ function getNow () {
   const dateTime = date + ' ' + time
   return dateTime
 }
-/*
+
 function Email (from, to, subject, content) {
   this.from = from
   this.to = to
@@ -184,8 +83,18 @@ function Email (from, to, subject, content) {
   this.content = content
 }
 */
-const d = getNow()
 export default {
+  filters: {
+    durationFormat (value) {
+      if (value) {
+        const dayPart = value.split('T')
+        const hourPart = dayPart[1].substring(0, 8)
+        return dayPart[0] + '  ' + hourPart
+      } else {
+        return 'unknown'
+      }
+    }
+  },
   layout: 'dashboard',
   data () {
     return {
@@ -193,17 +102,11 @@ export default {
       overlay: true,
       snackbar: false,
       snackbarMsg: '',
-      mailList: [
-        { from: { address: 'coleeeee@sfu.com', name: 'Abby' }, to: { address: 'abbbbby@sfu.com' }, date: d, subject: 'Event today', content: 'There is a wonderful basketball game on the indoor basketball court today' },
-        { from: { address: 'coleeeee@sfu.com', name: 'Cole' }, to: { address: 'abbbbby@sfu.com' }, date: d, subject: 'Good news', content: 'Cheese has dropped in price!' },
-        { from: { address: 'kennnnn@sfu.com' }, to: { address: 'abbbbby@sfu.com' }, date: d, subject: 'Important evet', content: 'Whatever else humans are supposed to eat' }
-      ]
+      mailList: []
     }
   },
   async created () {
-    console.log('id: ' + this.$currentID.value)
     this.mailList = await this.$client.emails.list(this.$currentID.value)
-    console.log('id: ')
     this.overlay = false
   },
   methods: {
@@ -217,11 +120,15 @@ export default {
     create () {
       // const email = new Email()
     },
+    read (id) {
+      // console.log(id)
+      // await this.$client.emails.show(id)
+    },
     async deleteEmail (id) {
       this.overlay = true
       try {
         await this.$client.emails.delete(id)
-        this.personas = await this.$client.emails.list()
+        this.personas = await this.$client.emails.list(this.$currentID.value)
       } catch (error) {
         this.snackbarMsg = 'An error occurred, please try again'
         this.snackbar = true
